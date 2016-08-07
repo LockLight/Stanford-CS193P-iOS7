8947a4248161ff78d6adb482bb77036d4c41033a
LockLight <locklight@locklightdemacbook-air.local>
2016-08-07 20:45:28 +0800

    Initial Commit

---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by locklight on 16/8/7.
+//  Copyright © 2016年 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardGame
+//
+//  Created by locklight on 16/8/7.
+//  Copyright © 2016年 CS193p. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
